64dd90c53dcb7541872c4b85c0136d0e05520634
Cedric Blanchard <the_atthis@proton.me>
2024-10-29 14:30:05 +0100

    mise en place du css

---
+++ tailwindconf.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        white: '#FAFAFA',
+        lime: '#D8E54E',
+        green: '#758000',
+        purple: '#642CA9',
+        pink: '#FF36AB',
+      }
+    }
+  }
+}
